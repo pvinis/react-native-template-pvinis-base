3d22b5fefd48b5a9c86b3fcbce1386966ba1deb6
Pavlos Vinieratos <pvinis@green.home>
2018-06-26 20:31:39 +0200

    update comments

--- ios/HelloWorld/AppDelegate.m
+++ ios/HelloWorld/AppDelegate.m
@@ -1,9 +1,10 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+//
+//  AppDelegate.m
+//  HelloWorld
+//
+//  Created by Pavlos Vinieratos on 26/06/2018.
+//  Copyright © 2018 pvin.is. All rights reserved.
+//
 
 #import "AppDelegate.h"
 
